48b7784cf5de38d6fd2b7d78e234e3396d445bf5
Harpreet Singh <harpres7@amdocs.com>
2026-03-08 15:54:31 +0530

    Initial Commit

--- services/transcript.service.js
+++ services/transcript.service.js
@@ -1,9 +1,11 @@
 import { exec } from "child_process";
 import fs from "fs";
-export const getTranscript = async (videoId) => {
 
+export const getTranscript = async (videoId) => {
   return new Promise((resolve, reject) => {
+
     const url = `https://www.youtube.com/watch?v=${videoId}`;
+
     const command = `yt-dlp \
 --cookies cookies.txt \
 --skip-download \
@@ -15,22 +17,22 @@ export const getTranscript = async (videoId) => {
 --sleep-requests 3 \
 --retries 5 \
 --fragment-retries 5 \
---compat-options no-youtube-prefer-utc \
 --extractor-args "youtube:player_client=android,web_safari,tv;youtube:skip=webpage" \
 -o "temp_%(id)s.%(ext)s" \
 ${url}`;
+
     exec(command, (error, stdout, stderr) => {
+
       console.log("STDOUT:", stdout);
       console.log("STDERR:", stderr);
 
-      const subtitleFile = `temp_${videoId}.en.vtt`;
+      // find any subtitle file generated
+      const files = fs.readdirSync(".");
+      const subtitleFile = files.find(
+        f => f.startsWith(`temp_${videoId}`) && f.endsWith(".vtt")
+      );
 
-      if (error && !fs.existsSync(subtitleFile)) {
-        return reject(new Error(stderr || error.message));
-      }
-
-
-      if (!fs.existsSync(subtitleFile)) {
+      if (!subtitleFile) {
         return reject(new Error("Subtitle file not found"));
       }
 
@@ -41,11 +43,12 @@ ${url}`;
         .replace(/Kind:.*?\n/g, "")
         .replace(/Language:.*?\n/g, "")
         .replace(/\d{2}:\d{2}:\d{2}\.\d{3} --> .*?\n/g, "")
-        .replace(/<[^>]+>/g, "")   // removes <c>, timestamps, etc
+        .replace(/<[^>]+>/g, "")
         .replace(/align:start position:0%/g, "")
         .replace(/\n/g, " ")
         .replace(/\s+/g, " ")
         .trim();
+
       fs.unlinkSync(subtitleFile);
 
       resolve(text);
